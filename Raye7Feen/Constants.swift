a6c9f1c48880bab32bf147df7aa54d3038a16d6a
Mahmoud Maamoun <iMaamoun@APPLEs-MacBook-Pro.local>
2017-12-09 21:36:46 +0200

    Go AnyWhere From Anywhere And Show Path on Google Maps

---
+++ Raye7Feen/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Raye7Feen
+//
+//  Created by Mahmoud Maamoun on 12/8/17.
+//  Copyright © 2017 Mahmoud Maamoun. All rights reserved.
+//
+
+import UIKit
+
+class Constants: NSObject {
+
+}
